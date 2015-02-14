0098755c190c2e959a2e421f729acfb1ae014be6
fsonmezay <git@ferdisonmezay.com>
2015-02-14 12:37:09 +0200

    delete comments, modift

--- examples/upload.php
+++ examples/upload.php
@@ -1,17 +1,4 @@
 <?php
-
-/**
- * WARNING!
- *
- * This file serves only for demonstration purposes. For using in production, please remember to think about security.
- */
-/*
-?>
-<form enctype="multipart/form-data" action="?" method="POST">
-	<input type="file" name="file">
-	<input type="submit"></button>
-</form>
-<?php */
 if(empty($_FILES['file']))
 {
 	exit();	
@@ -19,3 +6,4 @@ if(empty($_FILES['file']))
 $destination = 'uploads/'. $_FILES['file']['name'];
 move_uploaded_file($_FILES['file']['tmp_name'], $destination);
 echo $destination;
+?>
